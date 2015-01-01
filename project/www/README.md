1f14a6d5bc671f4f755cadb55d71cb172ccb3e41
Hamid FzM <hamidfzm@gmail.com>
2015-01-01 17:14:30 +0330

    adding web interface base files for gold miner

---
+++ project/www/README.md
@@ -0,0 +1,5 @@
+Gold Miner Web Interface
+=====
+
+Here we provide accounting system for users who wanted to use crawled data in web interface or using raspberry pi SoB.
+The web interface is available at : http://miner.rishe.co/
---
+++ project/www/config.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# python import
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    def __init__(self):
+        pass
+
+    SECRET_KEY = os.environ.get('SECRET_KEY_MINER', 'Development')
+
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Hamid FzM]'
+    FLASKY_MAIL_SENDER = 'Hamid FzM <mail@hamidfzm.ir>'
+    FLASKY_ADMIN = 'Hamid FzM'
+
+    LANGUAGES = {
+        'en': 'English',
+        'fa': u'فارسی'
+    }
+
+    BABEL_DEFAULT_LOCALE = 'fa'
+    BABEL_DEFAULT_TIMEZONE = 'UTC+03:30'
+
+    MONGODB_SETTINGS = {'DB': 'miner',
+                        'HOST': '127.0.0.1',
+                        'PORT': 27017}
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+config = {'development': DevelopmentConfig,
+          'production': ProductionConfig,
+          'default': DevelopmentConfig}
