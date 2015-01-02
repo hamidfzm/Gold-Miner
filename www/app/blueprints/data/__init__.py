18818186214cb1591c0fa7bc201ec6b23a619fdb
Hamid FzM <hamidfzm@gmail.com>
2015-01-02 22:58:30 +0330

    fix data blueprint address

--- www/app/blueprints/data/__init__.py
+++ www/app/blueprints/data/__init__.py
@@ -2,6 +2,6 @@ __author__ = 'hamid'
 
 from flask import Blueprint
 
-mod = Blueprint('data', __name__, url_prefix='/')
+mod = Blueprint('data', __name__, url_prefix='/data')
 
 from app.blueprints.data import views
