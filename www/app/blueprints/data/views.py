b7bbf4f205b9796cb6bad5a912e36fffa26a4ae2
Hamid FzM <hamidfzm@gmail.com>
2015-01-02 22:58:44 +0330

    add webview for tgju crawled data

--- www/app/blueprints/data/views.py
+++ www/app/blueprints/data/views.py
@@ -1,1 +1,23 @@
 __author__ = 'hamid'
+
+# python imports
+from mongoengine.connection import get_connection
+
+# flask imports
+from flask import jsonify
+from bson.json_util import _json_convert
+
+# project imports
+from . import mod
+
+
+@mod.route('/api/v1/tgju/', methods=['GET'])
+def get():
+    db = get_connection()['tgju']
+    data = dict()
+    for collection in db.collection_names():
+        if collection == 'system.indexes':
+            continue
+        data[collection] = _json_convert(db[collection].find())
+
+    return jsonify(**data)
